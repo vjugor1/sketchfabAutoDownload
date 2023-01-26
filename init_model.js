ea2180bc7dda82ffebacd62df30ac8ea5a6bde96
SashaLukashevich <vjugor1@gmail.com>
2023-01-26 16:20:36 +0300

    2 position

--- init_model.js
+++ init_model.js
@@ -10,9 +10,7 @@ var error = function error() {
 };
 function saveBase64AsFile(base64, fileName) {
     var link = document.createElement("a");
-
     document.body.appendChild(link); // for Firefox
-
     link.setAttribute("href", base64);
     link.setAttribute("download", fileName);
     link.click();
@@ -21,77 +19,63 @@ var success = function success(api) {
     var target = [0.0, 0.0, 0.0];
     var currentCamera = 0;
     var _loop;
+    var _podzaloop;
+
     _loop = function loop() {
-        currentCamera++;
-        console.log('=> Camera loop ', cameraPosition[currentCamera % 8].eye, target);
-        // api.getScreenShot('image/png', function (err, result) {
-        //     if (!err) {
-        //         window.console.log(result); // 'data:image/png;base64,iVBORw0KG...'
-        //     }
-        // });
+        api.setCameraLookAt([2, 0, 0], target, 0, function (err) {
+            if (err) console.error(err);
+            console.log('=> Camera Start Callback hardcode side');
+        });
         api.getScreenShot(800, 800, 'image/png', function (err, result) {
+
             var anchor = document.createElement('a');
             anchor.href = result;
             anchor.download = 'screenshot.png';
             anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
-            console.log(result)
-            // document.getElementById('controls').appendChild(anchor);
+            saveBase64AsFile(result, 'tst1.jpg')
+
         });
 
-        api.setCameraLookAt(cameraPosition[currentCamera % 8].eye, target, 0, function (err) {
-            if (err) console.error(err);
-            console.log('=> Camera Start Callback');
-        });
-        api.setCameraLookAtEndAnimationCallback(function (err) {
-            if (err) console.error(err);
-            console.log('=> Camera End Callback');
-            setTimeout(_loop, 500);
-        });
-        console.log(currentCamera);
-        api.setCameraEasing(easings[Math.floor(Math.random() * easings.length)]);
     };
+    _podzaloop = function podazoop() {
+        api.setCameraLookAt([0, -2, 0], target, 0, function (err) {
+            if (err) console.error(err);
+            console.log('=> Camera Start Callback hardcode side');
+        });
+
+        api.getScreenShot(800, 800, 'image/png', function (err, result) {
+
+            var anchor = document.createElement('a');
+            anchor.href = result;
+            anchor.download = 'screenshot.png';
+            anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
+
+            saveBase64AsFile(result, 'tst2.jpg')
+
+        });
+    };
+
+
     api.start(function () {
         api.addEventListener('viewerready', function () {
-            currentCamera++;
-            console.log('=> Camera loop ', cameraPosition[currentCamera % 8].eye, target);
-            api.setCameraLookAt(cameraPosition[currentCamera % 8].eye, target, 0, function (err) {
+
+
+            api.setCameraLookAt([2, 0, 0], target, 0, function (err) {
                 if (err) console.error(err);
-                console.log('=> Camera Start Callback');
+                console.log('=> Camera Start Callback hardcode front');
             });
-            // api.getScreenShot('image/png', function (err, result) {
-            //     if (!err) {
-            //         window.console.log(result); // 'data:image/png;base64,iVBORw0KG...'
-            //     }
-            // });
             api.getScreenShot(800, 800, 'image/png', function (err, result) {
+
                 var anchor = document.createElement('a');
                 anchor.href = result;
                 anchor.download = 'screenshot.png';
                 anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
-                saveBase64AsFile(result,'tst.jpg')
-                // document.getElementById('controls').appendChild(anchor);
-            });
-            // download https://sketchfab.com/developers/viewer/examples?sample=Screenshot%20Save
+                saveBase64AsFile(result, 'tst3.jpg');
 
-            api.getRootMatrixNode(function (err, nodeID) {
-                var direction = 0;
-                setInterval(function () {
-                    console.log('translate start');
-                    api.translate(nodeID, [direction * 1, 0, 0, 1], {
-                        duration: 15,
-                        easing: easings[Math.floor(Math.random() * easings.length)]
-                    }, function () {
-                        console.log('translate callback');
-                    });
-                    console.log('rotate start');
-                    api.rotate(nodeID, [Math.PI * 0.05 * direction, 0, 1, 0], {
-                        duration: direction === -1 ? 2 : 8
-                    }, function () {
-                        console.log('rotate callback');
-                    });
-                }, 1000);
             });
-            setTimeout(_loop, 500);
+
+            setTimeout(_loop, 0);
+
         });
     });
 };
@@ -104,10 +88,13 @@ client.init(uid, {
 });
 easings = ['easeLinear', 'easeOutQuad', 'easeInQuad', 'easeInOutQuad', 'easeOutCubic', 'easeInCubic', 'easeInOutCubic', 'easeOutQuart', 'easeInQuart', 'easeInOutQuart', 'easeOutQuintic', 'easeInQuintic', 'easeInOutQuintic', 'easeOutSextic', 'easeInSextic', 'easeInOutSextic', 'easeOutSeptic', 'easeInSeptic', 'easeInOutSeptic', 'easeOutOctic', 'easeInOctic', 'easeInOutOctic', 'easeOutBack', 'easeInBack', 'easeInOutBack', 'easeOutCircle', 'easeInCircle', 'easeInOutCircle', 'easeOutElastic', 'easeInElastic', 'easeInOutElastic', 'easeOutBounce', 'easeInBounce', 'easeInOutBounce'];
 cameraPosition = [{
-    eye: [2, 0, 0]
-}, {
-    eye: [2, 0, 0]
+    eye: [0, -2, 0]
 }, {
     eye: [0, -2, 0]
+}, {
+    eye: [2, 0, 0]
+},
+{
+    eye: [2, 0, 0]
 }
 ];
