6e47bff3e8595f2a425e3dda64e5d9d87335e588
SashaLukashevich <vjugor1@gmail.com>
2023-01-30 15:07:44 +0300

    4 position screenshot

--- init_model.js
+++ init_model.js
@@ -15,67 +15,56 @@ function saveBase64AsFile(base64, fileName) {
     link.setAttribute("download", fileName);
     link.click();
 }
+
 var success = function success(api) {
     var target = [0.0, 0.0, 0.0];
     var currentCamera = 0;
     var _loop;
-    var _podzaloop;
-
     _loop = function loop() {
-        api.setCameraLookAt([2, 0, 0], target, 0, function (err) {
+        currentCamera++;
+        console.log("currentCamera = ", currentCamera);
+        console.log('=> Camera loop ', cameraPosition[currentCamera % cameraPosition.length].eye, target);
+        api.setCameraLookAt(cameraPosition[currentCamera % cameraPosition.length].eye, target, 4, function (err) {
             if (err) console.error(err);
-            console.log('=> Camera Start Callback hardcode side');
+            console.log('=> Camera Start Callback');
         });
-        api.getScreenShot(800, 800, 'image/png', function (err, result) {
-
-            var anchor = document.createElement('a');
-            anchor.href = result;
-            anchor.download = 'screenshot.png';
-            anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
-            saveBase64AsFile(result, 'tst1.jpg')
-
-        });
-
-    };
-    _podzaloop = function podazoop() {
-        api.setCameraLookAt([0, -2, 0], target, 0, function (err) {
+        api.setCameraLookAtEndAnimationCallback(function (err) {
             if (err) console.error(err);
-            console.log('=> Camera Start Callback hardcode side');
-        });
-
-        api.getScreenShot(800, 800, 'image/png', function (err, result) {
-
-            var anchor = document.createElement('a');
-            anchor.href = result;
-            anchor.download = 'screenshot.png';
-            anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
-
-            saveBase64AsFile(result, 'tst2.jpg')
-
-        });
-    };
-
-
-    api.start(function () {
-        api.addEventListener('viewerready', function () {
-
-
-            api.setCameraLookAt([2, 0, 0], target, 0, function (err) {
-                if (err) console.error(err);
-                console.log('=> Camera Start Callback hardcode front');
-            });
+            console.log('=> Camera End Callback');
             api.getScreenShot(800, 800, 'image/png', function (err, result) {
 
                 var anchor = document.createElement('a');
                 anchor.href = result;
                 anchor.download = 'screenshot.png';
                 anchor.innerHTML = '<img width="100" height="100" src=' + result + '>download';
-                saveBase64AsFile(result, 'tst3.jpg');
+                saveBase64AsFile(result, 'tst.jpg')
 
             });
-
-            setTimeout(_loop, 0);
-
+            setTimeout(_loop, 5000);
+        });
+        api.setCameraEasing(easings[Math.floor(Math.random() * easings.length)]);
+    };
+    api.start(function () {
+        api.addEventListener('viewerready', function () {
+            api.getRootMatrixNode(function (err, nodeID) {
+                var direction = 1;
+                setInterval(function () {
+                    // console.log('translate start');
+                    // api.translate(nodeID, [direction * 1, 0, 0, 1], {
+                    //     duration: 1,
+                    //     easing: easings[Math.floor(Math.random() * easings.length)]
+                    // }, function () {
+                    //     console.log('translate callback');
+                    // });
+                    // console.log('rotate start');
+                    // api.rotate(nodeID, [Math.PI * 0.05 * direction, 0, 1, 0], {
+                    //     duration: direction === -1 ? 2 : 8
+                    // }, function () {
+                    //     console.log('rotate callback');
+                    // });
+                }, 1000);
+            });
+            setTimeout(_loop, 5000);
         });
     });
 };
@@ -83,10 +72,9 @@ client.init(uid, {
     success: success,
     error: error,
     preload: 1,
-    autotsart: 1,
-    camera: 0
+    autotsart: 1
 });
-// easings = ['easeLinear', 'easeOutQuad', 'easeInQuad', 'easeInOutQuad', 'easeOutCubic', 'easeInCubic', 'easeInOutCubic', 'easeOutQuart', 'easeInQuart', 'easeInOutQuart', 'easeOutQuintic', 'easeInQuintic', 'easeInOutQuintic', 'easeOutSextic', 'easeInSextic', 'easeInOutSextic', 'easeOutSeptic', 'easeInSeptic', 'easeInOutSeptic', 'easeOutOctic', 'easeInOctic', 'easeInOutOctic', 'easeOutBack', 'easeInBack', 'easeInOutBack', 'easeOutCircle', 'easeInCircle', 'easeInOutCircle', 'easeOutElastic', 'easeInElastic', 'easeInOutElastic', 'easeOutBounce', 'easeInBounce', 'easeInOutBounce'];
+easings = ['easeLinear', 'easeOutQuad', 'easeInQuad', 'easeInOutQuad', 'easeOutCubic', 'easeInCubic', 'easeInOutCubic', 'easeOutQuart', 'easeInQuart', 'easeInOutQuart', 'easeOutQuintic', 'easeInQuintic', 'easeInOutQuintic', 'easeOutSextic', 'easeInSextic', 'easeInOutSextic', 'easeOutSeptic', 'easeInSeptic', 'easeInOutSeptic', 'easeOutOctic', 'easeInOctic', 'easeInOutOctic', 'easeOutBack', 'easeInBack', 'easeInOutBack', 'easeOutCircle', 'easeInCircle', 'easeInOutCircle', 'easeOutElastic', 'easeInElastic', 'easeInOutElastic', 'easeOutBounce', 'easeInBounce', 'easeInOutBounce'];
 cameraPosition = [{
     eye: [0, -2, 0]
 }, {
